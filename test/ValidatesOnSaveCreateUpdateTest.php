516106ae4b552d55e8d39928579b357b8331cc4d
Koen Punt <koen@koenpunt.nl>
2013-04-28 21:21:18 +0000

    updated tests

--- test/ValidatesOnSaveCreateUpdateTest.php
+++ test/ValidatesOnSaveCreateUpdateTest.php
@@ -1,6 +1,4 @@
 <?php
-include 'helpers/config.php';
-
 
 class BookOn extends ActiveRecord\Model
 {
@@ -11,7 +9,7 @@ class BookOn extends ActiveRecord\Model
 
 class ValidatesOnSaveCreateUpdateTest extends DatabaseTest{
 
-	public function test_on_create()
+	public function test_validations_only_run_on_create()
 	{
 		BookOn::$validates_presence_of[0] = array('name', 'on' => 'create');
 		$book = new BookOn();
@@ -26,9 +24,8 @@ class ValidatesOnSaveCreateUpdateTest extends DatabaseTest{
 		$book->name = null;
 		$this->assert_true( $book->save() );
 	}
-	
-	
-	public function test_on_update()
+
+	public function test_validations_only_run_on_update()
 	{
 		BookOn::$validates_presence_of[0] = array('name', 'on' => 'update');
 		$book = new BookOn();
@@ -40,8 +37,8 @@ class ValidatesOnSaveCreateUpdateTest extends DatabaseTest{
 		$book->name = 'Baz';
 		$this->assert_true( $book->save() );
 	}
-	
-	public function test_on_save()
+
+	public function test_validations_only_run_on_save()
 	{
 		BookOn::$validates_presence_of[0] = array('name', 'on' => 'save');
 		$book = new BookOn();
@@ -56,4 +53,21 @@ class ValidatesOnSaveCreateUpdateTest extends DatabaseTest{
 		$book->name = null;
 		$this->assert_false( $book->save() );
 	}
+
+	public function test_validations_run_always_without_on()
+	{
+		BookOn::$validates_presence_of[0] = array('name');
+		$book = new BookOn();
+		$this->assert_false($book->is_valid());
+		
+		$book->secondary_author_id = 1;
+		$this->assert_false( $book->save() );
+		
+		$book->name = 'Baz';
+		$this->assert_true( $book->save() );
+		
+		$book->name = null;
+		$this->assert_false( $book->save() );
+	}
+	
 }
